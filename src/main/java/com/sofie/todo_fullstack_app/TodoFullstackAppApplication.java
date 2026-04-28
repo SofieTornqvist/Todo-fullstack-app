d8ce9d90e3c0cd298a06bf3a59ed79d06d159a83
SofieTornqvist <sofietornqvist@hotmail.com>
2026-04-28 09:02:28 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/sofie/todo_fullstack_app/TodoFullstackAppApplication.java
@@ -0,0 +1,13 @@
+package com.sofie.todo_fullstack_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoFullstackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoFullstackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofie/todo_fullstack_app/TodoFullstackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofie.todo_fullstack_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoFullstackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
